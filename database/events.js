e8a048e616044f2a1f686e068d885e0b6f9e156d
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-05-31 16:13:33 -0700

    changes

--- database/events.js
+++ database/events.js
@@ -7,95 +7,94 @@ mongoose.connect(mongoDB);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-db.once('open', function callback() {
-  const eventSchema = mongoose.Schema({
-    id: {type: String, unique: true},
-    name: String,
-    start: String,
-    end: String,
-    url: String,
-    address: String,
-    city: String,
-    image: String,
-  
-  });
-  
-  const Event = mongoose.model('Events', eventSchema);
-  
-  const venues = [];
-  
-  const getVenues = (venue) => {
-    let venueObj = JSON.parse(venue);
-    let venueObjInserted = false;
-    for (var i = 0; i < venues.length; i++) {
-      if (venues[i].id === venueObj.id) {
-        venueObjInserted = true;
-      }
-    }
-    if (!venueObjInserted) {
-      venues.push(JSON.parse(venue));
-    }
-  }
-  
-  let save = (events) => {
-    let event;
-    let venueID;
-    let venueAddress;
-    let venueCity;
-  
-    for (var i = 0; i < events.length; i++) {
-      venueID = events[i].venue_id;
-      for (var j = 0; j < venues.length; j++) {
-        if (venues[j].id === venueID) {
-          venueAddress = venues[j].address.address_1;
-          venueCity = venues[j].address.city;
-  
-          if (events[i].logo === null) {
-            events[i].logo = {url: 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png'}
-          }
-  
-          if (venueAddress === null) {
-            venueAddress = 'The venue to be confirmed later. Stay tuned!'
-          }
-  
-          event = new Event ({
-            id: events[i].id,
-            name: events[i].name.text,
-            start: events[i].start.local,
-            end: events[i].end.local,
-            url: events[i].url,
-            address: venueAddress,
-            city: venueCity,
-            image: events[i].logo.url
-          })
-  
-          event.save((err) => {
-            // if (err) return console.error(err);
-          });
-        }
-      }
-    }
-  }
-  
-  let get = (callback, sort) => {
-    if (sort === 'relevance') {
-      Event.find((err, events) => {
-        if (err) return console.log('ERROR on Find on relevance sort', err);
-        callback(events);
-      })
-    } else {
-      Event.find((err, events) => {
-        if (err) return console.log('ERROR on Find on date sort', err);
-        callback(events);
-      })
-      .sort({ start: 1 })
-    }
-  }
+const eventSchema = mongoose.Schema({
+  id: {type: String, unique: true},
+  name: String,
+  start: String,
+  end: String,
+  url: String,
+  address: String,
+  city: String,
+  image: String,
 
-  module.exports.save = save;
-  module.exports.get = get;
-  module.exports.getVenues = getVenues;
-})
+});
+
+const Event = mongoose.model('Events', eventSchema);
+
+const venues = [];
+
+const getVenues = (venue) => {
+  let venueObj = JSON.parse(venue);
+  let venueObjInserted = false;
+  for (var i = 0; i < venues.length; i++) {
+    if (venues[i].id === venueObj.id) {
+      venueObjInserted = true;
+    }
+  }
+  if (!venueObjInserted) {
+    venues.push(JSON.parse(venue));
+  }
+}
+
+let save = (events) => {
+  let event;
+  let venueID;
+  let venueAddress;
+  let venueCity;
+
+  for (var i = 0; i < events.length; i++) {
+    venueID = events[i].venue_id;
+    for (var j = 0; j < venues.length; j++) {
+      if (venues[j].id === venueID) {
+        venueAddress = venues[j].address.address_1;
+        venueCity = venues[j].address.city;
+
+        if (events[i].logo === null) {
+          events[i].logo = {url: 'https://cdn.evbstatic.com/s3-build/perm_001/f8c5fa/django/images/discovery/default_logos/4.png'}
+        }
+
+        if (venueAddress === null) {
+          venueAddress = 'The venue to be confirmed later. Stay tuned!'
+        }
+
+        event = new Event ({
+          id: events[i].id,
+          name: events[i].name.text,
+          start: events[i].start.local,
+          end: events[i].end.local,
+          url: events[i].url,
+          address: venueAddress,
+          city: venueCity,
+          image: events[i].logo.url
+        })
+
+        event.save((err) => {
+          // if (err) return console.error(err);
+        });
+      }
+    }
+  }
+}
+
+let get = (callback, sort) => {
+  if (sort === 'relevance') {
+    Event.find((err, events) => {
+      if (err) return console.log('ERROR on Find on relevance sort', err);
+      callback(events);
+    })
+  } else {
+    Event.find((err, events) => {
+      if (err) return console.log('ERROR on Find on date sort', err);
+      callback(events);
+    })
+    .sort({ start: 1 })
+  }
+}
+
+module.exports.save = save;
+module.exports.get = get;
+module.exports.getVenues = getVenues;
+
 
 
 
--- worker.js
+++ worker.js
@@ -10,74 +10,73 @@ mongoose.connect(mongoDB);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-db.once('open', function callback() {
-  const getEventsFromEB = () => {
-    let events = {
-      url: `
-      https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EventBrightToken}
-      &location.address=${'San Francisco'}
-      &q=${'blockchain'}
-      `,
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': `Bearer ${process.env.EventBrightToken}`
-      }
-    };
-  
-    return new Promise ((resolve, reject) => {
-      rp(events)
-        .then((events) => {
-          events = JSON.parse(events).events;
-          database.save(events);
-          return events;
-        })
-        .then((events) => {
-          let promises = [];
-  
-          for (var i = 0; i < events.length; i++) {
-            let venue = JSON.parse(events[i].venue_id);
-            let location = {
-              url: `https://www.eventbriteapi.com/v3/venues/${venue}/?token=${process.env.EventBrightToken}`,
-              headers: {
-                'User-Agent': 'request',
-                'Authorization': `Bearer ${process.env.EventBrightToken}`
-              }
+const getEventsFromEB = () => {
+  let events = {
+    url: `
+    https://www.eventbriteapi.com/v3/events/search/?token=${process.env.EventBrightToken}
+    &location.address=${'San Francisco'}
+    &q=${'blockchain'}
+    `,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `Bearer ${process.env.EventBrightToken}`
+    }
+  };
+
+  return new Promise ((resolve, reject) => {
+    rp(events)
+      .then((events) => {
+        events = JSON.parse(events).events;
+        database.save(events);
+        return events;
+      })
+      .then((events) => {
+        let promises = [];
+
+        for (var i = 0; i < events.length; i++) {
+          let venue = JSON.parse(events[i].venue_id);
+          let location = {
+            url: `https://www.eventbriteapi.com/v3/venues/${venue}/?token=${process.env.EventBrightToken}`,
+            headers: {
+              'User-Agent': 'request',
+              'Authorization': `Bearer ${process.env.EventBrightToken}`
             }
-            let locationPromise = rp(location)
-              .then((location) => {
-                database.getVenues(location);
-              })
-              promises.push(locationPromise);
           }
-          Promise.all(promises)
-            .then(resolve)
-            .catch((err) => {
-              reject(err)
-            });
-        })
-    })
-  }
-  
-  // const getEventsFromMeetup = () => {
-  //   let options = {
-  //     // url: `https://api.meetup.com/2/events?key=${config.MeetupToken}&group_urlname=ny-tech&sign=true`,
-  //     url: `https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&page=20&text=blockchain&key=${config.MeetupToken}`,
-  //     // url: `https://api.meetup.com/find/upcoming_events?key=${config.MeetupToken}&photo-host=public&text='blockchain'&zip=94102&radius=10&sign=true`,
-  //     headers: {
-  //       'User-Agent': 'request',
-  //       'Authorization': `Bearer ${config.MeetupToken}`
-  //     }
-  //   };
-  
-  //   request(options, (error, response, body) => {
-  //     console.log('error:', error); 
-  //     console.log('statusCode:', response.statusCode);
-  //     console.log('body:', body);
-  //   });
-  // }
-  
-  module.exports.getEventsFromEB = getEventsFromEB;
-  // module.exports.getEventsFromMeetup = getEventsFromMeetup;
-})
+          let locationPromise = rp(location)
+            .then((location) => {
+              database.getVenues(location);
+            })
+            promises.push(locationPromise);
+        }
+        Promise.all(promises)
+          .then(resolve)
+          .catch((err) => {
+            reject(err)
+          });
+      })
+  })
+}
+
+// const getEventsFromMeetup = () => {
+//   let options = {
+//     // url: `https://api.meetup.com/2/events?key=${config.MeetupToken}&group_urlname=ny-tech&sign=true`,
+//     url: `https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public&page=20&text=blockchain&key=${config.MeetupToken}`,
+//     // url: `https://api.meetup.com/find/upcoming_events?key=${config.MeetupToken}&photo-host=public&text='blockchain'&zip=94102&radius=10&sign=true`,
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': `Bearer ${config.MeetupToken}`
+//     }
+//   };
+
+//   request(options, (error, response, body) => {
+//     console.log('error:', error); 
+//     console.log('statusCode:', response.statusCode);
+//     console.log('body:', body);
+//   });
+// }
+
+module.exports.getEventsFromEB = getEventsFromEB;
+// module.exports.getEventsFromMeetup = getEventsFromMeetup;
+
 
 
